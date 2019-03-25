de101316a61b2a29253b191a2d8c01e097392f4d
Andy Fleener <andrew.fleener@sportngin.com>
2019-03-25 15:21:32 -0500

    Fix bundle install, drop rails 3.2 support

---
+++ install_bundler.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$RAILS_VERSION" == "4.2" ]
+then
+  gem uninstall -v ">= 2" -i $(rvm gemdir)@global -ax bundler || true
+  gem install bundler -v "< 2" gem install bundler -v "< 2"
+else
+  gem install bundler
+fi
